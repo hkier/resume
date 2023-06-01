6d79dd232b9047cbdab17a9d8a8401b51343a707
howard@mmpix.com <howard@mmpix.com>
2023-06-01 14:38:21 -0500

    added missing alt tags

--- src/components/Education.js
+++ src/components/Education.js
@@ -13,21 +13,7 @@ import Mustang from '../images/Education/Mustang.png'
 import fullstack from '../images/Education/fullstack.jpg'
 import Syracuse from '../images/Education/SU.jpg'
 
-// function BasicExample() {
-//   return (
-//     <Card className='mediumcard'>
-//       <Card.Img variant="top" src="holder.js/100px180" />
-//       <Card.Body>
-//         <Card.Title>Card Title</Card.Title>
-//         <Card.Text>
-//           Some quick example text to build on the card title and make up the
-//           bulk of the card's content.
-//         </Card.Text>
-//         <Button variant="primary">Go somewhere</Button>
-//       </Card.Body>
-//     </Card>
-//   );
-// }
+
 const Education = () => {
     return (
         <div id='education'>
